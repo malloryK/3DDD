da053ce5a7634bafff438d3c829e4757f5b5f2a2
Mallory Ketcheson <mallory.ketcheson@gmail.com>
2015-01-28 01:29:43 -0500

    Can Now Rotate shape by grabbing

--- New Unity Project/Assets/Resources/Scripts/DrawMesh.cs
+++ New Unity Project/Assets/Resources/Scripts/DrawMesh.cs
@@ -16,9 +16,14 @@ public class DrawMesh : MonoBehaviour {
 	HandModel leftRigid;
 
 	public GameObject prefabShape;
-	GameObject DrawnShape; 
+	GameObject drawnShape; 
+
+	Vector3 lastPinchSpot;
 
 
+	//float ANGULAR_DRAG = 10; //drag when spinning shape
+	float CONFIDENCE_TO_DRAW = 0.1f; // confidence needed that hand is pointing to add to mesh
+	float CONFIDENCE_TO_GRAB = 0.3f;
 	public HandController handController;
 		
 
@@ -28,20 +33,16 @@ public class DrawMesh : MonoBehaviour {
 		Verts = new List<Vector3>();
 		Tris = new List<int> ();
 
-		ClearShape ();
-	
-		
-		
-		
+		//ClearShape ();
 	}
 
-	void ClearShape(){
+	void NewShape(Vector3 position){
 
 		Points.Clear ();
 		Verts.Clear ();
 		Tris.Clear ();
 
-		DrawnShape =  Instantiate (prefabShape,new Vector3 (0.5f, 0, 0) , Quaternion.identity ) as GameObject;
+		drawnShape =  Instantiate (prefabShape,position , Quaternion.identity ) as GameObject;
 
 		CreateMesh ();
 		
@@ -51,11 +52,12 @@ public class DrawMesh : MonoBehaviour {
 	void Update () {
 		//Start new shape
 		if (Input.GetKeyDown (KeyCode.N)) {
-			ClearShape();
+			drawnShape = null;
 		}
 
 		//get hands 
 		rightHand = handController.GetFrame().Hands.Rightmost;	
+		//rightRigid = handController.GetAllPhysicsHands ()[0];
 		leftHand = handController.GetFrame ().Hands.Leftmost;
 		if (!rightHand.IsRight) {
 			rightHand = null;
@@ -64,43 +66,92 @@ public class DrawMesh : MonoBehaviour {
 				leftHand = null;
 		} 
 
-		//draw if there is a right hand
-		if(rightHand != null){
-
-			DrawOnIndexPoint();
-
+		// if there is a right hand and we're confident of its state
+		if(rightHand != null && rightHand.Confidence > CONFIDENCE_TO_DRAW){
+			//check just the index is pointing
+			if ( rightHand.Fingers[0].IsExtended == false && rightHand.Fingers[1].IsExtended == true &&  rightHand.Fingers[2].IsExtended == false && rightHand.Fingers[3].IsExtended == false && rightHand.Fingers[4].IsExtended == false)
+			{
+				DrawOnIndexPoint();
+			}else if(rightHand.GrabStrength > CONFIDENCE_TO_GRAB){
+				RotateDrawnShape();
+			}else {
+				lastPinchSpot = Vector3.zero;
+			}
+		}else {
+			lastPinchSpot = Vector3.zero;
 		}
+		
+	}
+
+	void RotateDrawnShape ()
+	{	
+
+		Vector3 pinchSpot = drawnShape.transform.position-(handController.transform.TransformPoint(rightHand.Fingers[1].TipPosition.ToUnityScaled ()));
+		if(lastPinchSpot != Vector3.zero){
+		
+			float angleAroundZ = Vector3.Angle (new Vector3(lastPinchSpot.x,lastPinchSpot.y,0), new Vector3(pinchSpot.x, pinchSpot.y, 0));
+			float angleAroundX = Vector3.Angle (new Vector3(0,lastPinchSpot.y,lastPinchSpot.z), new Vector3(0,pinchSpot.y, pinchSpot.z));
+			float angleAroundY = Vector3.Angle (new Vector3(lastPinchSpot.x,0,lastPinchSpot.z), new Vector3(pinchSpot.x,0,pinchSpot.z));
+
+			//vector.Angle gives acute angle- use sign to determine if change is necesary
+			float signZ = Mathf.Sign(Vector3.Dot(new Vector3(0,0,1),Vector3.Cross(new Vector3(lastPinchSpot.x,lastPinchSpot.y,0), new Vector3(pinchSpot.x, pinchSpot.y, 0))));
+			float signX = Mathf.Sign(Vector3.Dot(new Vector3(1,0,0),Vector3.Cross(new Vector3(0,lastPinchSpot.y,lastPinchSpot.z), new Vector3(0,pinchSpot.y, pinchSpot.z))));
+			float signY = Mathf.Sign(Vector3.Dot(new Vector3(0,1,0),Vector3.Cross(new Vector3(lastPinchSpot.x,0,lastPinchSpot.z), new Vector3(pinchSpot.x,0,pinchSpot.z))));
+
+			if(signZ < 0){
+				signZ = 360-signZ;
+			}
+			if(signX < 0){
+				signX = 360-signX;
+			}
+			if(signY < 0){
+				signY = 360-signY;
+			}
+
+	
+			drawnShape.transform.RotateAround (drawnShape.transform.position, new Vector3(0,0,1), angleAroundZ );
+			drawnShape.transform.RotateAround (drawnShape.transform.position, new Vector3(1,0,0), angleAroundX );
+			drawnShape.transform.RotateAround (drawnShape.transform.position, new Vector3(0,1,0), angleAroundY );
+		}
+		lastPinchSpot = pinchSpot;
+
+
+
 	}
 
 	void DrawOnIndexPoint(){
 		//check just the index is pointing
-		if ( rightHand.Fingers[0].IsExtended == false && rightHand.Fingers[1].IsExtended == true &&  rightHand.Fingers[2].IsExtended == false && rightHand.Fingers[3].IsExtended == false && rightHand.Fingers[4].IsExtended == false)
-		{
-			//draw
-			Vector3 drawPosition = handController.transform.TransformPoint(rightHand.PalmPosition.ToUnityScaled());
-			Finger index = rightHand.Fingers[1];
-			Points.Add (DrawnShape.transform.InverseTransformPoint(handController.transform.TransformPoint(index.JointPosition(Finger.FingerJoint.JOINT_TIP).ToUnityScaled())));
-			Points.Add (DrawnShape.transform.InverseTransformPoint(handController.transform.TransformPoint(index.JointPosition(Finger.FingerJoint.JOINT_PIP).ToUnityScaled())));
-			Points.Add (DrawnShape.transform.InverseTransformPoint(handController.transform.TransformPoint(index.JointPosition(Finger.FingerJoint.JOINT_MCP).ToUnityScaled())));
-			UpdateMesh();
 			
-		}
+
+			//draw
+			Finger index = rightHand.Fingers[1];
+
+			if(drawnShape == null){
+				NewShape(handController.transform.TransformPoint(index.TipPosition.ToUnityScaled()));
+			}
+
+			Points.Add (drawnShape.transform.InverseTransformPoint(handController.transform.TransformPoint(index.JointPosition(Finger.FingerJoint.JOINT_TIP).ToUnityScaled())));
+			Points.Add (drawnShape.transform.InverseTransformPoint(handController.transform.TransformPoint(index.JointPosition(Finger.FingerJoint.JOINT_PIP).ToUnityScaled())));
+			Points.Add (drawnShape.transform.InverseTransformPoint(handController.transform.TransformPoint(index.JointPosition(Finger.FingerJoint.JOINT_MCP).ToUnityScaled())));
+			UpdateMesh();
+
+		
 	}
 
 
 
 	private void CreateMesh(){
 
-		DrawnShape.AddComponent ("MeshFilter");
-		DrawnShape.AddComponent ("MeshRenderer");
-		DrawnShape.AddComponent ("MeshCollider");
+		drawnShape.AddComponent ("MeshFilter");
+		drawnShape.AddComponent ("MeshRenderer");
+		drawnShape.AddComponent ("MeshCollider");
 		mat = Resources.Load ("Materials/Default") as Material;
 		if(mat == null){
 			Debug.LogError ("Materials not found.");
 			return;
 		}
 
-		MeshFilter meshFilter = DrawnShape.GetComponent<MeshFilter> ();
+		MeshFilter meshFilter = drawnShape.GetComponent<MeshFilter> ();
 		if (meshFilter == null) {
 			Debug.LogError("MeshFilter Not found.");
 			return;
@@ -112,14 +163,17 @@ public class DrawMesh : MonoBehaviour {
 			mesh = meshFilter.sharedMesh;
 		}
 
-		MeshCollider meshCollider = DrawnShape.GetComponent<MeshCollider> ();
+		MeshCollider meshCollider = drawnShape.GetComponent<MeshCollider> ();
 		if (meshCollider == null) {
 			Debug.LogError("MeshCollider not found.");
 			return;
 		}
 		meshCollider.convex = true;
-		DrawnShape.AddComponent("Rigidbody");
-		DrawnShape.rigidbody.useGravity = false;
+		drawnShape.AddComponent("Rigidbody");
+		drawnShape.rigidbody.useGravity = false;
+		//drawnShape.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+		//drawnShape.rigidbody.angularDrag = ANGULAR_DRAG; 
+		drawnShape.collider.isTrigger = true;
 
 	}
 
@@ -147,12 +201,12 @@ public class DrawMesh : MonoBehaviour {
 			mesh.vertices = Verts.ToArray();
 			mesh.triangles = Tris.ToArray();
 
-			MeshCollider meshCollider = DrawnShape.GetComponent<MeshCollider>();
+			MeshCollider meshCollider = drawnShape.GetComponent<MeshCollider>();
 			mesh.RecalculateNormals ();
 			mesh.RecalculateBounds ();
 			meshCollider.sharedMesh = null;
 			meshCollider.sharedMesh = mesh;
-			DrawnShape.renderer.material = mat;
+			drawnShape.renderer.material = mat;
 			mesh.Optimize();
 
 
